ccb38a6c984e4ba3f2c0f65a41d2ad683b45d946
Donald Jones <other214@gmail.com>
2013-02-05 09:04:59 -0500

    Improve Layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,12 +5,12 @@ describe "StaticPages" do
   describe "Home Pages" do
 
     it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => 'Sample App')
     end
 
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title',
                 :content => "Ruby on Rails Tutorial Sample App | Home")
     end
@@ -20,12 +20,12 @@ describe "StaticPages" do
   describe "Help page" do
 
     it "should have the h1 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title',
                 :content => "Ruby on Rails Tutorial Sample App | Help")
     end
@@ -35,12 +35,12 @@ describe "StaticPages" do
   describe "About page" do
 
     it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title',
                 :content => "Ruby on Rails Tutorial Sample App | About Us")
     end
@@ -50,12 +50,12 @@ describe "StaticPages" do
   describe "Contact page" do
 
     it "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', :text => 'Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title',
                 :content => "Ruby on Rails Tutorial Sample App | Contact")
     end
