b3ccbc14b348070a8546e1bac50dbcc3e67544de
Donald Jones <other214@gmail.com>
2013-02-11 12:36:13 -0500

    Improve User Model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,12 +13,18 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com",
+		     password: "foobar", password_confirmation: "foobar") 
+  end
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
 
@@ -36,5 +42,25 @@ describe User do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
+
+  describe "when email address is already taken" do
+    before do 
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+   end
+
+   describe "when password in not present" do
+     before { @user.password = @user.password_confirmation = " " }
+     it { should_not be_valid }
+   end
+
+   describe "when passwords doesn't match confirmationt" do
+     before { @user.password_confirmation = "mismatch" }
+     it { should_not be_valid }
+   end
 
 end
