539966a0c2c4aef614e97fa972e91d509a5216e6
Donald Jones <other214@gmail.com>
2013-02-28 11:30:18 -0500

    imp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class UsersController < ApplicationController
 
   def show
@@ -28,3 +29,42 @@ class UsersController < ApplicationController
   end
 
 end
+=======
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+ 
+  def new
+    @user = User.new
+  end
+ 
+
+  def new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:user])
+    if @user.update_attributes(params[:user]}    
+      # Handle a successful update
+    else
+      render 'edit'
+    end
+  end
+end
+>>>>>>> d84efda254b4529ab0589d9cb75219197ef81224
