bfdafab35618f4e00355a6bdec537ba23bc188c7
Donald Jones <other214@gmail.com>
2013-03-05 06:54:32 -0500

    imp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
-<<<<<<< HEAD
 class UsersController < ApplicationController
+  before_filter  :signed_in_user, only:  [:edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -9,10 +10,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
  
-
-  def new
-  end
-
   def create
     @user = User.new(params[:user])
     if @user.save
@@ -25,46 +22,29 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.fin(params[:id])
-  end
-
-end
-=======
-class UsersController < ApplicationController
-
-  def show
-    @user = User.find(params[:id])
-  end
- 
-  def new
-    @user = User.new
-  end
- 
-
-  def new
-  end
-
-  def create
-    @user = User.new(params[:user])
-    if @user.save
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-    else
-      render 'new'
-    end
-  end
-
-  def edit
-    @user = User.find(params[:id])
+    #@user = User.fin(params[:id])
   end
 
   def update
     @user = User.find(params[:user])
-    if @user.update_attributes(params[:user]}    
-      # Handle a successful update
+    if @user.update_attributes(params[:user])    
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
     else
       render 'edit'
     end
   end
+
+  private
+
+    def signed_in_user
+      redirect_to signin_path,  notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end  
+
 end
->>>>>>> d84efda254b4529ab0589d9cb75219197ef81224
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -75,7 +75,10 @@ describe "User pages" do
   
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
-    before { visit edit_user_path(user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
 
     describe "page" do
       it { should have_selector('h1',   text: "Update your profile") }
@@ -86,8 +89,26 @@ describe "User pages" do
     describe "with invalid information" do
       before { click_button "Save changes" }
 
-       it { should have_content('error') }
+      it { should have_content('error') }
     end
-  end
+    
+    describe "with valid information" do
+      let(:new_name)   { "New Name" }
+      let(:new_email)  { "new@example.com" }
+      before do
+        fill_in "Name",            with: new_name
+        fill_in "Email",           with: new_email
+        fill_in "Password",        with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end
+
+      it { should have_selector('title', text: new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', href: signout_path) }
+      specity { user.reload.name.should == new.name }
+      specity { user.reload.email.should == new.email }
+    end
+  end    
 
 end
