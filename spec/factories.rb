8e7dd45d456d4518b23c852e951403d16cf3d730
Donald Jones <other214@gmail.com>
2013-03-07 17:23:57 -0500

    imp

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :user do
     sequence(:name)  { |n| "Person #{n}" }
     sequence(:email) { |n| "person_#{n}@example.com" }
-    email "michael@example.com"
     password "foobar"
     password_confirmation "foobar"
   
