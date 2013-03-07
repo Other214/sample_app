490d71dcbe1e859080c89f435dc3c4cf91489d99
Donald Jones <other214@gmail.com>
2013-03-07 08:30:06 -0500

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:user])
+    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])    
       flash[:success] = "Profile updated"
       sign_in @user
